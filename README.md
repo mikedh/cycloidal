31ffc53978aba0b7e226956473d3d321cbcae0cf
Michael Dawson-Haggerty <mikedh@kerfed.com>
2020-10-14 12:42:12 -0400

    update docstrings

--- README.md
+++ README.md
@@ -7,10 +7,14 @@ Generate profiles for [cycloidal drives](https://en.wikipedia.org/wiki/Cycloidal
 
 The profile generation was implemented from 'Gear geometry of cycloid drives', Chen BingKui et al.' and inspired by a [post on zincland](http://www.zincland.com/hypocycloid). 
 
-Numpy is required, matplotlib is recommended. Try tuning the dimension and count parameters in generate_regular. generate_rolling generates the profiles for a more hypothetical but related mechanism. 
+Install requirements with:
+
+```
+pip install trimesh[easy] matplotlib
+```
 
 Run with:
 
-    
-    ipython -i cycloidal.py
-    
+```    
+ipython -i cycloidal.py
+```
--- cycloidal.py
+++ cycloidal.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python
-'''
+"""
 Mike Dawson-Haggerty
 1/30/2014
 
@@ -10,7 +9,7 @@ Implemented from:
 'Gear geometry of cycloid drives', Chen BingKui et al.
 http://download.springer.com/static/pdf/96/art%253A10.1007%252Fs11431-008-0055-3.pdf
 Equations 10, 11
-'''
+"""
 
 import numpy as np
 
@@ -18,50 +17,60 @@ import trimesh
 
 
 def cycloidal_profile(count_pin,
-                      count_cam,
                       eccentricity,
                       radius_pin,
                       radius_pattern,
+                      count_cam=None,
                       resolution=16):
-    '''
+    """
     Return a (n,2) curve representing a cyloidal gear profile.
 
     Parameters
     -------------
-    count_pin:      int, number of pins in the radial pattern
-    count_cam:      int, number of lobes on the disc.
-                          for a regular disc, equal to count_pin-1
-    eccentricity:   float, magnitude of eccentricity
-    radius_pin:     float, radius of individual pin
-    radius_pattern: float, radius of pin centers
-    resolution:     float, number of points per degree
+    count_pin : int
+      Number of pins in the radial pattern
+    eccentricity : float
+      Magnitude of eccentricity
+    radius_pin : float
+      Radius of individual pin
+    radius_pattern : float
+      Radius of pin centers
+    count_cam : None or int
+      Number of lobes on the disc.
+      If None set to a regular disc value: `count_pin - 1`
+    resolution : float
+      Number of points per degree
 
     Returns
     ------------
-    profile: (n,2) float, ordered points on curve in 2D space
-    '''
-    Rz = radius_pattern  # radius of pin pattern
-    rz = radius_pin      # radius of pin
-    e = eccentricity    # eccentricity
-    Zb = count_pin       # number of pins
-    Zg = count_cam       # tooth count on gear
+    profile : (n, 2) float
+      Ordered points on curve in 2D space
+    """
+    # confirm data types of input
+    Rz = float(radius_pattern)  # radius of pin pattern
+    rz = float(radius_pin)     # radius of pin
+    e = float(eccentricity)    # eccentricity
+    Zb = int(count_pin)        # number of pins
+
+    # tooth count on gear
+    if count_cam is None:
+        Zg = count_pin - 1
+    else:
+        Zg = int(count_cam)
 
     Ze = Zb / (Zb - Zg)
     Zd = Zg / (Zb - Zg)
     K1 = (e * Zb) / (Rz * (Zb - Zg))
-
-    # in the paper they say you should calculate this numerically...
-    psi = np.linspace(0, np.pi * 2, int(resolution * 360))
-
+    # in the paper they say you should calculate this numerically
+    psi = np.linspace(0, np.pi * 2, int(resolution * 360),
+                      dtype=np.float64)
     denom_B = np.sqrt(1 + K1**2 - 2 * K1 * np.cos(Zd * psi))
     cos_B = np.sign(Zb - Zg) * ((K1 * np.sin(Ze * psi)) -
                                 np.sin(psi)) / denom_B
     sin_B = np.sign(Zb - Zg) * ((-K1 * np.cos(Ze * psi)) +
                                 np.cos(psi)) / denom_B
-
     x = Rz * np.sin(psi) - e * np.sin(Ze * psi) + rz * cos_B
     y = Rz * np.cos(psi) - e * np.cos(Ze * psi) - rz * sin_B
-
     profile = np.column_stack((x, y))
 
     return profile
@@ -74,36 +83,45 @@ def dual_cycloidal(eccentricity=.07,
                    input_count=3,
                    input_radius=.5625,
                    input_pattern=1.3125):
-    '''
+    """
     Generate the profiles, pins, and holes for a regular dual- disc
     cycloidal drive. This design has two discs operation 180 degrees
     out of phase to minimize vibration.
 
     Parameters
     ------------
-    eccentricity:   float, magnitude of eccentricity
-    count_pin: int, number of fixed pins
-    radius_pin:     float, radius of a fixed pin
-    radius_pattern: float, radius of pin pattern
-    input_count:    int, number of holes in the cycloidal disc
-    input_radius:   float, radius of the holes in the cycloidal disc
-    input_pattern:  float, radius of the hole pattern in the disc
+    eccentricity : float
+      Magnitude of eccentricity
+    count_pin : int
+      Number of fixed pins
+    radius_pin : float
+      Radius of a fixed pin
+    radius_pattern : float
+      Radius of pin pattern
+    input_count : int
+      Number of holes in the cycloidal disc
+    input_radius : float
+      Radius of the holes in the cycloidal disc
+    input_pattern :  float
+      Radius of the hole pattern in the disc
 
     Returns
     -------------
-    drive: Path2D object, with two disc layers and a pin layer
-    '''
+    drive : trimesh.path.Path2D
+      With two disc layers and a pin layer
+    """
 
     # half a tooth spacing for transforming disc to pin pattern
     spacing = .5 * np.pi / (count_pin - 1)
 
     # get a disc profile, with a very dense sampling
-    a = trimesh.load_path(cycloidal_profile(count_pin=count_pin,
-                                            count_cam=count_pin - 1,
-                                            eccentricity=eccentricity,
-                                            radius_pin=radius_pin,
-                                            radius_pattern=radius_pattern,
-                                            resolution=32))
+    a = trimesh.load_path(cycloidal_profile(
+        count_pin=count_pin,
+        count_cam=count_pin - 1,
+        eccentricity=eccentricity,
+        radius_pin=radius_pin,
+        radius_pattern=radius_pattern,
+        resolution=32))
 
     # replace the polyline entity with a bajillion points with a
     # tightly fit B-Spline
@@ -123,32 +141,54 @@ def dual_cycloidal(eccentricity=.07,
     b.apply_layer('cam_disc_B')
 
     # generate the fixed pins
-    pins = trimesh.path.creation.circle_pattern(pattern_radius=radius_pattern,
-                                                circle_radius=radius_pin,
-                                                count=count_pin)
+    pins = trimesh.path.creation.circle_pattern(
+        pattern_radius=radius_pattern,
+        circle_radius=radius_pin,
+        count=count_pin)
     pins.apply_layer('pins')
 
-    # add the holes for the
-    holes_A = trimesh.path.creation.circle_pattern(pattern_radius=input_pattern,
-                                                   circle_radius=input_radius,
-                                                   center=[-eccentricity, 0.0],
-                                                   count=input_count)
+    # add the holes for the input bearings and set layers
+    holes_A = trimesh.path.creation.circle_pattern(
+        pattern_radius=input_pattern,
+        circle_radius=input_radius,
+        center=[-eccentricity, 0.0],
+        count=input_count)
     holes_A.apply_layer('cam_disc_A')
-
-    holes_B = trimesh.path.creation.circle_pattern(pattern_radius=input_pattern,
-                                                   circle_radius=input_radius,
-                                                   center=[eccentricity, 0.0],
-                                                   count=input_count)
+    holes_B = trimesh.path.creation.circle_pattern(
+        pattern_radius=input_pattern,
+        circle_radius=input_radius,
+        center=[eccentricity, 0.0],
+        count=input_count)
     holes_B.apply_layer('cam_disc_B')
 
     # concatenate all of the paths into a single drawing
-    drive = a + b + pins + holes_A + holes_B
+    drive = trimesh.path.util.concatenate(
+        [a, b, pins, holes_A, holes_B])
 
     return drive
 
 
 if __name__ == '__main__':
-    drive = dual_cycloidal()
+
+    radius_pin = .125
+    radius_pattern = 2.5
+    count_pin = 24
+    # set eccentricity as a fraction of pin radius
+    eccentricity = radius_pin * 0.56
+    input_pattern = radius_pattern - 1.25
+
+    # get the Path2D of this profile
+    drive = dual_cycloidal(
+        eccentricity=eccentricity,
+        count_pin=count_pin,
+        radius_pin=radius_pin,
+        radius_pattern=radius_pattern,
+        input_count=3,
+        input_radius=.5625,
+        input_pattern=input_pattern)
+
+    # export as a DXF
     drive.export('cycloidal_drive.dxf')
 
+    # show the result
     drive.show()
